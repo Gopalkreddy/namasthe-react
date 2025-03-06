30122513014afb8922d6aed5710934fcd98cb148
Gopal <gopal.kuppireddy@tagnos.com>
2025-03-06 22:05:56 +0530

    used map

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ const RestaurantCard=(props)=>{
     console.log(props);
     return( 
                 <div className="restaurant-card" style={restStyle}>
-                   <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+resData.info.cloudinaryImageId} alt="restaurant"/>
+                   <img className="res-logo" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt="restaurant"/>
                     <h3>{name}</h3>
                     <h4>{cuisines.join(",")}</h4>
                     <h4>{avgRating}</h4>
@@ -811,7 +811,7 @@ const Body=()=>{
               <div className="restaurant-container">
                 {
                     restList.map((restaurant)=>{
-                        return <RestaurantCard resData={restaurant}/>
+                        return <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     })
                 }
                   
